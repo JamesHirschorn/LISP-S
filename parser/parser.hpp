8dd61084598bbe01fa21c31f803e3203c94bc3f7
JamesHirschorn <james.hirschorn@gmail.com>
2014-07-14 10:20:10 -0400

    backup

--- parser/parser.hpp
+++ parser/parser.hpp
@@ -1,7 +1,13 @@
 /* Class for parsing the input for tree summing. */
 
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
 #include <istream>
 #include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <string>
 
 #include "tree/tree.hpp"
 
@@ -28,7 +34,8 @@ private:
 	std::shared_ptr<char> _tree_buffer;
 };
 
-binary_tree_summing_parser::binary_tree_summing_parser(
+template<typename I>
+binary_tree_summing_parser<I>::binary_tree_summing_parser(
 	std::istream& is, 
 	std::size_t integer_buffer_size, 
 	std::size_t tree_buffer_size) : 
@@ -41,16 +48,72 @@ binary_tree_summing_parser::binary_tree_summing_parser(
 {
 }
 
-long long binary_tree_summing_parser::getInteger(int delim)
+template<typename I>
+typename binary_tree_summing_parser<I>::integer_type 
+binary_tree_summing_parser<I>::getInteger(int delim)
 {
+	// read up to but not including the delimiter
 	_is.get(_integer_buffer.get(), _integer_buffer_size, delim);
    
 	// convert the buffer to a long long
 	std::istringstream istr(_integer_buffer.get());
 
-	long long result;
+	integer_type result;
 	istr >> result;
 	return result;
 }
 
+template<typename I>
+tree::binary_tree<integer_type> 
+binary_tree_summing_parser<I>::getTree(std::istream& tree_stream, int left_delim, int right_delim)
+{
+	char error_message[] = "Bad tree input";
+
+	char c;
+	std::string saved_stream;
+
+	// keeps track of the balance between left and right delimiters (0 means they are matched)
+	long long count = 0;
+
+	bool read_left = false;	// whether the left delimiter has been seen at least once 
+	bool valid = false;		// whether tree is valid
+
+	// Discard the surrounding left_delim and right_delim,
+	// and save the middle stuff to saved_stream.
+	while (count >= 0 && (!read_left || count > 0) && tree_stream.get(c))
+	{
+		switch (c)
+		{
+		case left_delim:
+			++count;
+			if (read_left)
+				saved_stream += c;
+			else
+				read_left = true;
+			break;
+		case right_delim:
+			--count;
+			if (count > 0)
+				saved_stream += c;
+			break;
+		default:
+			saved_stream += c;
+			break;
+		}
+	}
+	
+	// Check if the tree input is valid.
+	if (!read_left || count != 0)
+		throw std::exception(error_message);
+
+	// We have parsed the empty tree.
+	if (saved_stream.is_empty())
+		return tree::binary_tree<I>();
+
+	binary_tree<integer_type> left_tree = getTree(saved_stream, left_delim, right_delim);
+	binary_tree<integer_type> 
+}
+
 }	// namespace parser
+
+#endif
